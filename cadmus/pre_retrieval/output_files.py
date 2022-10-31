220413b8b9fc92ff17d26e21de8a362358db3bf0
jamcam11 <jamcam11@live.com>
2022-10-31 17:04:45 +0000

    update to file structure

--- cadmus/pre_retrieval/output_files.py
+++ cadmus/pre_retrieval/output_files.py
@@ -1,22 +1,26 @@
 import os
 
 def output_files ():
-    #creating the directories we are planning on using the save the result of the system
+    #creating the directories we are planning on using to save the results of the retrieval system
     for path in ['output',
                 'output/retrieved_df',
                 'output/esearch_results',
                 'output/medline',
                 'output/medline/txts',
-                'output/medline/p',
                 'output/crossref',
-                'output/crossref/p',
                 'output/formats',
                 'output/formats/xmls',
                 'output/formats/pdfs',
                 'output/formats/htmls',
                 'output/formats/txts',
                 'output/formats/zips',
-                'output/formats/tgzs']:
+                'output/formats/tgzs'
+                'output/formats/xmls/parsed',
+                'output/formats/pdfs/parsed',
+                'output/formats/htmls/parsed',
+                'output/formats/txts/parsed',
+                'output/formats/zips/parsed',
+                'output/formats/tgzs/parsed']:
         try:
             #try to create the directory, most likely will work for new project
             os.mkdir(path)
